02ad527cccdeb5bc733093747549543550963b0a
Abhishek <abhipro01@gmail.com>
2024-12-22 19:08:20 +0530

    File Structure sorted

---
+++ Basics/Basics.ts
@@ -0,0 +1,8 @@
+const myName : string = "john wick";
+console.log(myName);
+
+const myFavNumber : number = 3;
+console.log(myFavNumber);
+
+const isLoggedIn : boolean = true;
+console.log(isLoggedIn)
