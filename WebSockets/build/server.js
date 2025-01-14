8601f091198c381758b59e042cba7f421c841dff
Abhishek <abhipro01@gmail.com>
2025-01-14 20:54:57 +0530

    Users connection in websockets

--- WebSockets/build/server.js
+++ WebSockets/build/server.js
@@ -3,12 +3,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
 const PORT = 8080;
 const wss = new ws_1.WebSocketServer({ port: PORT });
+const allSockets = [];
+let users = 0;
 wss.on("connection", (s) => {
-    console.log("WS connected");
-    setInterval(() => {
-        s.send("hi");
-    }, 1000);
-    s.on("message", () => {
-        s.send("hello");
+    console.log("user connected");
+    users += 1;
+    console.log(users);
+    allSockets.push(s);
+    s.on("message", (message) => {
+        allSockets.forEach(s => (s.send(message.toString())));
+    });
+    s.on("close", () => {
+        allSockets.filter(s => s != s);
+        console.log("user disconnected.");
     });
 });
--- WebSockets/src/server.ts
+++ WebSockets/src/server.ts
@@ -1,16 +1,26 @@
-import { WebSocketServer } from "ws";
+import { WebSocket, WebSocketServer } from "ws";
 
 
 const PORT = 8080
 const wss = new WebSocketServer({port:PORT});
 
+const allSockets : WebSocket[] = [];
+let users = 0;
 
 wss.on("connection",(s)=>{
-    console.log("WS connected");
-    setInterval(()=>{
-        s.send("hi")
-    },1000)
-    s.on("message",()=>{
-        s.send("hello")
+    console.log("user connected");
+    users+=1;
+    console.log(users);
+    
+    allSockets.push(s);
+
+    s.on("message",(message)=>{
+        allSockets.forEach(s => (s.send(message.toString())))
     })
+
+    s.on("close",()=>{
+        allSockets.filter(s => s!=s)
+        console.log("user disconnected.")
+    })
+
 })
