5d67ab537eeb8e8161af931b1ade80070ce7fe50
Abhishek <abhipro01@gmail.com>
2025-01-16 20:49:08 +0530

    room and chat logic added

--- WebSockets/build/server.js
+++ WebSockets/build/server.js
@@ -3,18 +3,26 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
 const PORT = 8080;
 const wss = new ws_1.WebSocketServer({ port: PORT });
-const allSockets = [];
 let users = 0;
+const allUsers = [];
 wss.on("connection", (s) => {
-    console.log("user connected");
-    users += 1;
-    console.log(users);
-    allSockets.push(s);
-    s.on("message", (message) => {
-        allSockets.forEach(s => (s.send(message.toString())));
-    });
-    s.on("close", () => {
-        allSockets.filter(s => s != s);
-        console.log("user disconnected.");
+    s.on("message", (m) => {
+        var _a;
+        const parssedMessage = JSON.parse(m.toString());
+        if (parssedMessage.type == "join") {
+            allUsers.push({
+                socket: s,
+                roomId: parssedMessage.payload.roomId
+            });
+        }
+        if (parssedMessage.type == "chat") {
+            let curRoom = (_a = allUsers.find(x => x.socket == s)) === null || _a === void 0 ? void 0 : _a.roomId;
+            console.log("Current Room :" + curRoom);
+            allUsers.forEach(x => {
+                if (x.roomId == curRoom) {
+                    x.socket.send(parssedMessage.payload.message);
+                }
+            });
+        }
     });
 });
--- WebSockets/src/server.ts
+++ WebSockets/src/server.ts
@@ -1,25 +1,34 @@
 import { WebSocket, WebSocketServer } from "ws";
 
+const PORT = 8080;
+const wss = new WebSocketServer({port:PORT})
 
-const PORT = 8080
-const wss = new WebSocketServer({port:PORT});
+let users = 0;
+interface User {
+    socket:WebSocket,
+    roomId:string
+}
 
-const allSockets : WebSocket[] = [];
+const allUsers : User[] = [];
 
 wss.on("connection",(s)=>{
-    console.log("user connected");
-    users+=1;
-    console.log(users);
-    
-    allSockets.push(s);
+    s.on("message",(m)=>{
+        const parssedMessage = JSON.parse(m.toString());
+        if(parssedMessage.type == "join"){
+            allUsers.push({
+                socket:s,
+                roomId:parssedMessage.payload.roomId
+            })
+        }
 
-    s.on("message",(message)=>{
-        allSockets.forEach(s => (s.send(message.toString())))
+        if(parssedMessage.type == "chat"){
+            let curRoom = allUsers.find(x => x.socket == s)?.roomId;
+            console.log("Current Room :"+curRoom);
+            allUsers.forEach(x => {
+                if(x.roomId == curRoom){
+                    x.socket.send(parssedMessage.payload.message)
+                }
+            })
+        }
     })
-
-    s.on("close",()=>{
-        allSockets.filter(s => s!=s)
-        console.log("user disconnected.")
-    })
-
 })
